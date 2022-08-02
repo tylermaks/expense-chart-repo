52aa674226f272c2fc9272bab3f5c15ccfd2e343
Tyler Maksymiw <tylermcodes@gmail.com>
2022-08-02 10:08:34 -0400

    Add balance and chart components

---
+++ src/Balance.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Balance() {
+    return(
+        <section className="balance-container">
+            <article>
+                <p>My Balance</p>
+                <h2>$921.48</h2>
+            </article>
+            <img className="logo" src="images/logo.svg" alt="Logo" />
+        </section>
+    )
+}
+
+export default Balance;
